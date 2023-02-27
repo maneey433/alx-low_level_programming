8351b57c934115f6a3649fc6945b04c9accbb947
Clement <cledans123@gmail.com>
2023-02-27 11:26:27 -0800

    puts function

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *_puts -> this is a function that puts 
- *@str: a param to _puts function
- */
+*_puts -> this is a function that puts 
+*@str: a param to _puts function
+*/
  *void _puts(char *str);
 {
-       for(; *str != '\0'; str++)
+       for (; *str != '\0'; str++)
        {
 	   _putchar(*str);
        }
